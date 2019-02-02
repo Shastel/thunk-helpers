f159b45babc862d7fd1a7f68bdd417e90ddf607b
shastel <max@shas.tel>
2019-02-03 01:15:41 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+function useWith(func, mapfns) {
+    return function(...args) {
+        const newArgs = args.map((arg, i) => mapfns[i] ? mapfns[i](arg) : arg);
+
+        func(...newArgs);
+    };
+}
+
+const identity = a => a;
+
+function handleAction(dispatch, action) {
+    if (null == action) {
+        return;
+    }
+
+    if (Array.isArray(action)) {
+        action.map((action) => handleAction(dispatch, action));
+    }
+}
+
+export function withDispath (action) {
+    return function(dispatch, getState, ...rest) {
+        const result = action(dispatch, getState, ...rest);
+
+        if (null == result) {
+            return;
+        }
+
+        if (result instanceof Promise) {
+            return result.then((action) => handleAction(dispatch, action));
+        }
+
+        if ('object' === typeof result) {
+            return dispatch(result);
+        }
+    };
+}
+
+export function withState (action) {
+    const actionWrapper = withDispath(action);
+
+    return useWith(actionWrapper, [ identity, f => f() ]);
+}
+
+export function withSelectors (action, selectors) {
+    const actionWrapper = withState(action);
+
+    if ('function' === typeof selectors) {
+        return useWith(actionWrapper, [ identity, (s) => selectors(s)]);
+    }
+
+    return useWith(actionWrapper, [ identity, (s) => selectors.map(selector => selector(s))]);
+}
